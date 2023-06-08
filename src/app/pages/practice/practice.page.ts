c6e708b70f0b34a763a8378847f106d97ebe0f81
diegogglez <diegomiller7@gmail.com>
2023-06-08 15:41:35 +0200

    feat: making the form submittable

--- src/app/pages/practice/practice.page.ts
+++ src/app/pages/practice/practice.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -8,13 +8,31 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './practice.page.html',
   styleUrls: ['./practice.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, ReactiveFormsModule]
 })
 export class PracticePage implements OnInit {
 
+  practiceForm: FormGroup;
+  drillShoots: number[] = [0, 1, 2, 3, 4, 5, 5];
+  
   constructor() { }
 
   ngOnInit() {
+    this.practiceForm = new FormGroup({
+      'two-1': new FormControl('0'),
+      'two-2': new FormControl('0'),
+      'two-3': new FormControl('0'),
+      'two-4': new FormControl('0'),
+      'two-5': new FormControl('0'),
+      'three-1': new FormControl('0'),
+      'three-2': new FormControl('0'),
+      'three-3': new FormControl('0'),
+      'three-4': new FormControl('0'),
+      'three-5': new FormControl('0'),
+    })
   }
 
+  onSubmit() {
+    console.log(this.practiceForm);
+  }
 }
