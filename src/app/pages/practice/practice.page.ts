32603f58d46a1a7649380cfe311a1d74d90952ed
diegogglez <diegomiller7@gmail.com>
2023-06-11 02:48:00 +0200

    feat: showing the last practice rates saved

--- src/app/pages/practice/practice.page.ts
+++ src/app/pages/practice/practice.page.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { PracticeFormComponent } from './practice-form/practice-form.component';
+import { Practice } from 'src/app/models/practice';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-practice',
@@ -16,18 +18,28 @@ import { PracticeFormComponent } from './practice-form/practice-form.component';
     PracticeFormComponent
   ],
 })
-export class PracticePage {
+export class PracticePage implements OnInit{
 
-  twoPointRateValue: number | null;
-  threePointRateValue: number | null;
+  public twoPointRateValue: number | null;
+  public threePointRateValue: number | null;
+  public history: Practice[] = [];
 
-  constructor() {}
+
+  constructor(private storageService: StorageService) {}
+
+  ngOnInit(): void {
+    this.getHistory();
+  }
+
+  async getHistory() {
+    this.history = await this.storageService.getPractices();
+  }
 
   twoPointRate(rate: any) {
-    this.twoPointRateValue = rate.toFixed(1);
+    this.twoPointRateValue = rate;
   }
 
   threePointRate(rate: any) {
-    this.threePointRateValue = rate.toFixed(1);
+    this.threePointRateValue = rate;
   }
 }
