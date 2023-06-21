42bc9eac64ca94293b0a845b46709cc198babd38
diegogglez <diegomiller7@gmail.com>
2023-06-21 13:45:53 +0200

    add drill-select component

---
+++ src/app/pages/practice/practice-form/drill-select/drill-select.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drill-select',
+  templateUrl: './drill-select.component.html',
+  styleUrls: ['./drill-select.component.scss'],
+})
+export class DrillSelectComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
