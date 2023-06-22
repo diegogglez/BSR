566c8d9adcc4cda5f7bcd7b8087e3dc47422207b
diegogglez <diegomiller7@gmail.com>
2023-06-22 21:38:08 +0200

    feat: layout and logic added to the custom select component

--- src/app/pages/practice/practice-form/drill-select/drill-select.component.ts
+++ src/app/pages/practice/practice-form/drill-select/drill-select.component.ts
@@ -1,14 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, ViewChild, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { IonSelect, IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-drill-select',
   templateUrl: './drill-select.component.html',
   styleUrls: ['./drill-select.component.scss'],
+  standalone: true,
+  imports: [
+    IonicModule, 
+    CommonModule, 
+    ReactiveFormsModule,
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DrillSelectComponent),
+      multi: true
+    }
+  ]
 })
-export class DrillSelectComponent  implements OnInit {
+export class DrillSelectComponent implements ControlValueAccessor{
+
+  options: number[] = [0, 1, 2, 3, 4, 5];
+  @ViewChild('drillSelect') select!: IonSelect;
+
+  @Input() selected: number;
+
+  public value: number;
+  public changed: (value: string) => void;
+  public touched: () => void;
+  public isDisabled: boolean;
 
   constructor() { }
 
-  ngOnInit() {}
+  writeValue(value: number): void {
+    this.value = value;
+  }
 
+  onChange(event: Event): void {
+    const value: string = (<HTMLSelectElement>event.target).value;
+    this.changed(value);
+  }
+
+  registerOnChange(fn: any): void {
+    this.changed = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.touched = fn
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }  
+
+  handleClick() {
+    this.select.open();
+  }
 }
