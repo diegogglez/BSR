74196f07362c068423f541e11719bec1c54cc069
diegogglez <diegomiller7@gmail.com>
2023-08-14 00:40:06 +0200

    feat: motivational messages added to the form

--- src/app/pages/practice/practice-form/drill-select/drill-select.component.ts
+++ src/app/pages/practice/practice-form/drill-select/drill-select.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, ViewChild, forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
-import { IonSelect, IonicModule } from '@ionic/angular';
+import { IonSelect, IonicModule, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-drill-select',
@@ -23,9 +23,16 @@ import { IonSelect, IonicModule } from '@ionic/angular';
 })
 export class DrillSelectComponent implements ControlValueAccessor{
 
-  options: number[] = [0, 1, 2, 3, 4, 5];
-  @ViewChild('drillSelect') select!: IonSelect;
+  public options: any[] = [
+    {value: 0, message: 'Keep trying'}, 
+    {value: 1, message: 'Ok'}, 
+    {value: 2, message: 'Good'}, 
+    {value: 3, message: 'Very Good'}, 
+    {value: 4, message: 'Nice!'}, 
+    {value: 5, message: 'Epic!!'}
+  ];
 
+  @ViewChild('drillSelect') select!: IonSelect;
   @Input() selected: number;
 
   public value: number;
@@ -33,7 +40,7 @@ export class DrillSelectComponent implements ControlValueAccessor{
   public touched: () => void;
   public isDisabled: boolean;
 
-  constructor() { }
+  constructor(private toastController: ToastController) { }
 
   writeValue(value: number): void {
     this.value = value;
@@ -42,6 +49,7 @@ export class DrillSelectComponent implements ControlValueAccessor{
   onChange(event: Event): void {
     const value: string = (<HTMLSelectElement>event.target).value;
     this.changed(value);
+    this.showToast(Number(value))
   }
 
   registerOnChange(fn: any): void {
@@ -58,5 +66,15 @@ export class DrillSelectComponent implements ControlValueAccessor{
 
   handleClick() {
     this.select.open();
+  }
+
+  async showToast(value: number) {
+    const toast = await this.toastController.create({
+      message: this.options[value].message,
+      duration: 1000,
+      position: 'middle',
+      cssClass: 'motivation'
+    });
+    await toast.present();
   }
 }
